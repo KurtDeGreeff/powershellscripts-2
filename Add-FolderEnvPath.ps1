58df8ccd90be1ad04c2e28f2a21b997ef41e9cf7
nightshade2109 <nightshade2109@gmail.com>
2016-08-18 22:31:38 +0200

    Update Add-FolderEnvPath.ps1

--- Add-FolderEnvPath.ps1
+++ Add-FolderEnvPath.ps1
@@ -1,37 +1,37 @@
 Function Add-FolderEnvPath
 {
   <#
-    .SYNOPSIS
-    Adds a path to the system enviroment path
+      .SYNOPSIS
+      Adds a path to the system environment path
 
-    .DESCRIPTION
-    Checks if the path is valid if so it will get the current enviroment system path and compares each path to the one which should be added if the path exists it will be added if not it will be skipped.
+      .DESCRIPTION
+      Checks if the path is valid if so it will get the current environment system path and compares each path to the one which should be added if the path exists it will be added if not it will be skipped.
 
-    .PARAMETER Version
-    Output script version
+      .PARAMETER Version
+      Output script version
 
-    .PARAMETER PathToAdd
-    Path to add
+      .PARAMETER Path
+      Path to add
 
-    .EXAMPLE
-    Add-FolderEnvPath -Version
-    Outputs the current script version to be used in future for automatic script updates
+      .EXAMPLE
+      Add-FolderEnvPath -Version
+      Outputs the current script version to be used in future for automatic script updates
 
-    .EXAMPLE
-    Add-FolderEnvPath -PathToAdd C:\Tools\Utilities
-    Adds the path to the system enviroment path
+      .EXAMPLE
+      Add-FolderEnvPath -Path C:\Tools\Utilities
+      Adds the path to the system environment path
 
-    .NOTES
-    Author: Bevin Du Plessis
+      .NOTES
+      Author: Bevin Du Plessis
 
-    .LINK
-    https://github.com/nightshade2109/powershellscripts
+      .LINK
+      https://github.com/nightshade2109/powershellscripts
 
-    .INPUTS
-    [string]
+      .INPUTS
+      [string]
 
-    .OUTPUTS
-    [string]
+      .OUTPUTS
+      [string]
   #>
 
 
@@ -40,8 +40,8 @@ Function Add-FolderEnvPath
   (    
     [Parameter(ParameterSetName = 'Version')]
     [switch]$Version,
-    [Parameter(Mandatory = $true,Position = 0,ValueFromPipeline = $true, ParameterSetName = 'PathToAdd')]
-    [string] $PathToAdd
+    [Parameter(Mandatory = $true,HelpMessage = 'Path to add',Position = 0,ValueFromPipeline = $true, ParameterSetName = 'Path')]
+    [string] $Path
   )
 
   Begin
@@ -58,6 +58,8 @@ Function Add-FolderEnvPath
       
       Write-Output -InputObject $VersionResult
     }
+    $VersionResult = $null
+    $ScriptVersion = $null
 
     if ($PSBoundParameters.ContainsKey('Debug'))
     {
@@ -68,50 +70,84 @@ Function Add-FolderEnvPath
     {
       $VerbosePreference = 'Continue'
     }
- 
-   
   }
+  
   Process
   {
     if(!($Version.IsPresent)) 
     {
       Write-Verbose -Message "Processing $($MyInvocation.Mycommand)"
-
-    
-      
       Try
       {
-      
-      
-        If(Test-Path -Path $PathToAdd) {
-      
-          Write-Verbose -Message "Path $PathToAdd is valid continuing"
-      
-        } else {
-          Write-Warning -Message "Path $PathToAdd is not valid"
+        If(Test-Path -Path $Path) 
+        {
+          Write-Verbose -Message "Path $Path is valid continuing"
+        } 
+        else 
+        {
+          Write-Warning -Message "Path $Path is not valid"
           break
         }
 
-        Write-Verbose -Message "Adding folder $PathToAdd to SYSTEM path"
+        Write-Verbose -Message "Adding folder $Path to system environment path"
         
-        [string[]]$PathArray = ($env:path).split(';')
+        $EnvPaths = $null
+        
+        [string[]]$EnvPaths = ($env:path).split(';')
         [bool]$PathAlreadyExists = $false
-        [string]$FinalPath = $null
-      
-        Foreach ($Path in $PathArray) 
+        [string]$FinalEnvPath = $null
+        
+            
+        # Build possible combinations of paths to check with or without slashes
+        $PathLastCharacter = $Path[-1]
+        if($PathLastCharacter -ieq [IO.Path]::DirectorySeparatorChar -or $PathLastCharacter -ieq [IO.Path]::AltDirectorySeparatorChar) 
         {
-          if($Path -ieq $PathToAdd) 
-          {
-            $PathAlreadyExists = $true
-                
-            Write-Verbose -Message "Path $Path is already added to system path"
+          $PathsToCheck += $Path.Substring(0,$Path.Length-1)
+          $PathsToCheck += $Path
+        }
+        else 
+        {
+          $PathsToCheck += $(Join-Path -Path $Path -ChildPath '\')
+          $PathsToCheck += $Path
+        }
+        
+        
+        
+      
+        Foreach ($EnvPath in $EnvPaths) 
+        {
+        
+          Foreach ($PathToCheck in $PathsToCheck) {
+          
+            if($EnvPath -ieq $PathToCheck) 
+            {
+              $PathAlreadyExists = $true
+              Write-Verbose -Message "Path $EnvPath is already added to system path"
+              break
+            }
+          
           }
+   
         }
       
         if(!($PathAlreadyExists))
         {
-          $PathArray += $PathToAdd
-          $FinalPath = ($PathArray -Join ';').replace(';;',';') # Fail safe in case there is a comma in the path passed to the script
+          $EnvPaths = $null
+          [char]$Char = ';'
+        
+        
+          $EnvPathLastCharacter = $env:path[-1]
+          if($EnvPathLastCharacter -eq $Char) 
+          {
+            Write-Verbose -Message 'Removing ; character from path'
+            $NewEnvPaths = $env:path.Substring(0,$env:path.Length-1)
+            Write-Verbose -Message "Updated path: $NewEnvPaths"
+          }
+        
+          [string[]]$EnvPaths = ($NewEnvPaths).split(';')
+                
+          $EnvPaths += $Path
+          $FinalEnvPath = ($EnvPaths -Join ';').replace(';;',';') # Fail safe in case there is a comma in the path passed to the script
         }
       }
       Catch
@@ -124,12 +160,14 @@ Function Add-FolderEnvPath
       
       Finally 
       {
-        if(!([string]::IsNullOrEmpty($FinalPath))) 
+        $Result = $null
+        
+        if(!([string]::IsNullOrEmpty($FinalEnvPath))) 
         {
           $Result = New-Object -TypeName PSObject -Property @{
-            Output = $FinalPath
+            Output = $FinalEnvPath
           }
-          [Environment]::SetEnvironmentVariable('path',$FinalPath, 'Machine')
+          [Environment]::SetEnvironmentVariable('Path',$FinalEnvPath, 'Machine')
         }
         else 
         {
@@ -138,19 +176,13 @@ Function Add-FolderEnvPath
           }
         }
         Write-Output -InputObject $Result
-        
-        $Result = $null
       }
-         
-      $VersionResult = $null
-      $ScriptVersion = $null
-      $NewPath = $null
-      $PathArray = $null
     }
 
   }
   End
   {
+    Update-EnvVarsForSession
     Write-Verbose -Message "Ending $($MyInvocation.Mycommand)"
   }
 }
